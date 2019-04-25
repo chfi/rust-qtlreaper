9d54b3d0187889d9c4567bfd96db7d97f5903351
Christian Fischer <christian@chfi.se>
2019-04-25 10:19:00 -0500

    functions for parsing metadata and strain genotypes

--- src/geneobject.rs
+++ src/geneobject.rs
@@ -5,6 +5,7 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::Path;
 
+#[derive(Debug)]
 pub struct Locus {
     pub name: String,
     pub chromosome: Chromosome,
@@ -16,12 +17,35 @@ pub struct Locus {
     pub mega_basepair: f64,
 }
 
+#[derive(Debug)]
 pub struct Chromosome {
     pub name: String,
     pub loci: Vec<Locus>,
     pub size: usize,
 }
 
+#[derive(Debug)]
+pub struct Metadata {
+    pub name: String,
+    pub maternal: String,
+    pub paternal: String,
+    pub heterozygous: String, // defaults to "H"
+    pub unknown: String,      // defaults to "U"
+}
+
+impl Metadata {
+    fn new(name: &str, maternal: &str, paternal: &str) -> Metadata {
+        Metadata {
+            name: String::from(name),
+            maternal: String::from(maternal),
+            paternal: String::from(paternal),
+            heterozygous: String::from("H"),
+            unknown: String::from("U"),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct Dataset {
     pub name: String,
     pub maternal: String,
@@ -37,6 +61,7 @@ pub struct Dataset {
     // pub interval: i32,
 }
 
+#[derive(Debug)]
 pub struct QTL {
     pub lrs: f64,
     pub additive: f64,
@@ -93,12 +118,61 @@ fn parse_tab_delim_line(line: &str) -> Vec<String> {
         .collect()
 }
 
-fn parse_tab_delim_line_2(line: &str) -> Vec<&str> {
-    line.split_terminator('\t')
-        // .map(|s| String::from(s))
-        .collect()
+// fn parse_metadata_line(line: &str) -> Option<(String, String)> {
+fn parse_metadata_line(line: &str) -> Option<(&str, &str)> {
+    if line.starts_with("#") {
+        return None;
+    }
+
+    if line.starts_with("@") {
+        let sep = line.find(':').unwrap();
+        let name = &line[1..sep];
+        let val = &line[sep + 1..];
+
+        return Some((name, val));
+    }
+
+    None
 }
 
+// panic!s if the provided lines do not contain @name, @mat, and @pat fields
+fn parse_metadata_lines(lines: Vec<&str>) -> Metadata {
+    let mut name: Option<String> = None;
+    let mut mat: Option<String> = None;
+    let mut pat: Option<String> = None;
+    let mut het = String::from("H");
+    let mut unk = String::from("U");
+
+    for line in lines.iter() {
+        if let Some((n, v)) = parse_metadata_line(line) {
+            match n {
+                "name" => name = Some(String::from(v)),
+                "mat" => mat = Some(String::from(v)),
+                "pat" => pat = Some(String::from(v)),
+                "het" => het = String::from(v),
+                "unk" => unk = String::from(v),
+                _ => (),
+            }
+        }
+    }
+
+    if name == None || mat == None || pat == None {
+        panic!(
+            "Required metadata was not provided!\nname = {:?}\nmat = {:?}\npat = {:?}",
+            name, mat, pat
+        );
+    }
+
+    Metadata {
+        name: name.unwrap(),
+        maternal: mat.unwrap(),
+        paternal: pat.unwrap(),
+        heterozygous: het,
+        unknown: unk,
+    }
+}
+
+#[derive(Debug)]
 struct DatasetHeader {
     has_cm: bool,
     strains: Vec<String>,
@@ -121,11 +195,59 @@ fn parse_header_line(line: &str) -> Option<DatasetHeader> {
         .collect();
 
     Some(DatasetHeader { has_cm, strains })
-    // match header_iter.next() {
-    //     None => return None;
+}
 
-    // }
-    // None
+#[derive(Debug, PartialEq)]
+struct DatasetLine<'a> {
+    chromosome: String,
+    locus: String,
+    centi_morgan: f64,
+    mega_basepair: Option<f64>,
+    genotypes: Vec<&'a str>,
+}
+
+fn parse_data_line<'a>(
+    header: &DatasetHeader,
+    metadata: &'a Metadata,
+    line: &str,
+) -> Option<DatasetLine<'a>> {
+    let words = parse_tab_delim_line(&line);
+
+    let chromosome = words.get(0).unwrap().clone();
+    let locus = words.get(1).unwrap().clone();
+    let centi_morgan = words.get(2).unwrap().parse::<f64>().unwrap();
+
+    let skip_n = if header.has_cm { 4 } else { 3 };
+    let mega_basepair = if header.has_cm {
+        words.get(3).map(|s| s.parse::<f64>().unwrap())
+    } else {
+        None
+    };
+
+    let geno_ref = |s| {
+        let mat = metadata.maternal.as_str();
+        let pat = metadata.paternal.as_str();
+        if s == mat {
+            mat
+        } else if s == pat {
+            pat
+        } else {
+            panic!(
+                "Locus {} has a genotype that's neither maternal nor paternal!",
+                locus
+            );
+        }
+    };
+
+    let genotypes = words.into_iter().skip(skip_n).map(geno_ref).collect();
+
+    Some(DatasetLine {
+        chromosome,
+        locus,
+        centi_morgan,
+        mega_basepair,
+        genotypes,
+    })
 }
 
 #[cfg(test)]
@@ -144,14 +266,8 @@ mod tests {
         String::from("1	D1Mit1	8.3	B6	B6	D	D")
     }
 
-    /*
-    let data_line2 = "1	D1Mit231	12	D	B6	B6	D";
-    let data_line3 = "1	D1Mit169	14.5	D	B6	B6	D";
-    let data_line3 = "1	D1Mit373	17	D	B6	B6	D";
-    let data_line3 = "1	D1Mit318	18.5	D	B6	B6	D"; */
-
     #[test]
-    fn parse_header() {
+    fn it_can_parse_header() {
         let header = header_line();
         let result = parse_tab_delim_line(&header);
 
@@ -175,16 +291,73 @@ mod tests {
     }
 
     #[test]
-    fn parse_data_lines() {
+    fn it_can_parse_data_lines() {
         let line = data_line1();
         let result = parse_tab_delim_line(&line);
 
         assert_eq!(vec!["1", "D1Mit1", "8.3", "B6", "B6", "D", "D"], result);
+
+        let header = parse_header_line(&header_line()).unwrap();
+        let metadata = Metadata {
+            name: String::from("BXD"),
+            maternal: String::from("B6"),
+            paternal: String::from("D"),
+            heterozygous: String::from("H"),
+            unknown: String::from("U"),
+        };
+        let parsed = parse_data_line(&header, &metadata, &line).unwrap();
+
+        assert_eq!(
+            DatasetLine {
+                chromosome: String::from("1"),
+                locus: String::from("D1Mit1"),
+                centi_morgan: 8.3,
+                mega_basepair: None,
+                genotypes: vec!["B6", "B6", "D", "D"],
+            },
+            parsed
+        );
+    }
+
+    #[test]
+    fn it_can_parse_metadata_lines() {
+        let lines = vec![
+            "#@type:intercross",
+            "@name:BXD",
+            "#abbreviation of maternal or paternal parents",
+            "@mat:B6",
+        ];
+
+        assert_eq!(parse_metadata_line(lines[0]), None);
+        assert_eq!(parse_metadata_line(lines[1]), Some(("name", "BXD")));
+        assert_eq!(parse_metadata_line(lines[2]), None);
+        assert_eq!(parse_metadata_line(lines[3]), Some(("mat", "B6")));
+    }
+
+    #[test]
+    fn it_can_parse_metadata() {
+        let lines = vec![
+            "#the first three/four columns should be \"Chr, Locus, cM, [Mb]\" (case sensitive)",
+        "#please save as Unix format text file.",
+        "#comment line always start with a '#'",
+        "#type riset or intercross",
+        "@type:riset",
+        "#@type:intercross",
+        "@name:BXD",
+        "#abbreviation of maternal or paternal parents",
+        "@mat:B6",
+        "@pat:D",
+        "#heterozygous , optional, default is \"H\"",
+        "@het:H",
+        "#Unknown , optional, default is \"U\"",
+        "@unk:U",
+        "Chr	Locus	cM	BXD1	BXD2	BXD5	BXD6	BXD8	BXD9	BXD11	BXD12	BXD13	BXD14	BXD15	BXD16	BXD18	BXD19	BXD20	BXD21	BXD22	BXD23	BXD24	BXD25	BXD27	BXD28	BXD29	BXD30	BXD31	BXD32	BXD33	BXD34	BXD35	BXD36	BXD37	BXD38	BXD39	BXD40	BXD42",
+            ];
+
+        println!("{:?}", parse_metadata_lines(lines));
     }
 
 }
-
-// fn parse_tab_delimited(filename: &str) ->
 
 /*
 impl Dataset {
